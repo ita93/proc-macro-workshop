f42332a2793cc2fc27cf7c630d4b6e2ccfd93ddd
Phi Nguyen <phind.uet@gmail.com>
2019-10-03 00:34:24 +0800

    Lab05 Token tree text for refe

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Data};
 use quote::quote;
 
 #[proc_macro_derive(Builder)]
@@ -9,13 +9,38 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let name = &ast.ident;
     let bname = format!("{}Builder",name);
+    let fields = if let Data::Struct(ds) = &ast.data {
+        ds.fields.clone()
+    } else {
+        unimplemented!()
+    };
+
+    let optionized = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote!{
+            #name: std::option::Option<#ty>
+        }
+    });
+
+    let fnized = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+
+        quote!{
+            pub fn #name(&mut self, #name: #ty) -> &mut Self{
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
     let bident = syn::Ident::new(&bname, name.span());
     let expaned = quote! {
+        use std::error::Error;
+
         struct #bident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#optionized,)*
         }
 
         impl #name {
@@ -30,7 +55,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
         
         impl #bident{
-            fn executable(&mut self, executable: String) -> &mut Self {
+            /*fn executable(&mut self, executable: String) -> &mut Self {
                 self.executable = Some(executable);
                 self
             }
@@ -49,6 +74,19 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 self.current_dir = Some(current_dir);
                 self
             }
+            */
+            #(#fnized)*
+
+            pub fn build(&mut self) -> Result<#name, Box<dyn Error>> {
+                Ok(
+                    #name {
+                        executable: self.executable.clone().ok_or("Missing executable argument")?,
+                        args: self.args.clone().ok_or("Missing args argument")?,
+                        env: self.env.clone().ok_or("Missing env argument")?,
+                        current_dir: self.current_dir.clone().ok_or("Missing current_dir argument")?,
+                    }
+                )    
+            }    
         }
     };
     expaned.into()

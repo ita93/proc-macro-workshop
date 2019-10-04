0850b086304b2c407e19fc57e5be85507c6145a0
Phi Nguyen <phind.uet@gmail.com>
2019-10-05 01:24:25 +0800

    Field attribute TODO: There is still a large of redundant code need to be removed. Next: Write Command::Builder as iterpolation

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,27 +1,34 @@
 extern crate proc_macro;
+extern crate proc_macro2;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{
-    parse_macro_input, AngleBracketedGenericArguments, Attribute, Data, DeriveInput, PathArguments,
-};
+use syn::{parse_macro_input, AngleBracketedGenericArguments, Data, DeriveInput, PathArguments};
 
-fn is_an_option(f: &syn::Field) -> Option<AngleBracketedGenericArguments> {
+enum InnerType {
+    VecInner(AngleBracketedGenericArguments),
+    OptionInner(AngleBracketedGenericArguments),
+    None,
+}
+
+fn get_inner(f: &syn::Field) -> InnerType {
     let ty = &f.ty;
 
     match ty {
         syn::Type::Path(val) => {
             let ty_ident = val.path.segments.first().unwrap().ident.to_string();
-            if ty_ident == "Option" {
-                if let PathArguments::AngleBracketed(angle_generic) =
-                    &val.path.segments.first().unwrap().arguments
-                {
-                    return Some(angle_generic.clone());
-                };
-            }
-            None
+            if let PathArguments::AngleBracketed(angle_generic) =
+                &val.path.segments.first().unwrap().arguments
+            {
+                if ty_ident == "Option" {
+                    return InnerType::OptionInner(angle_generic.clone());
+                } else if ty_ident == "Vec" {
+                    return InnerType::VecInner(angle_generic.clone());
+                }
+            };
+            InnerType::None
         }
-        _ => None,
+        _ => InnerType::None,
     }
 }
 
@@ -30,15 +37,23 @@ fn has_attribute(f: &syn::Field) -> Option<String> {
         Some(attr) => {
             match attr.parse_meta() {
                 Ok(syn::Meta::List(mtl)) => {
-                    let attr_path = mtl.path.get_ident().map_or(String::from("undifined"), |id| id.to_string());
+                    let attr_path = mtl
+                        .path
+                        .get_ident()
+                        .map_or(String::from("undifined"), |id| id.to_string());
                     assert_eq!(&attr_path, "builder"); //only accept builder
-                    //only accept builder
+                                                       //only accept builder
                     match mtl.nested.first() {
                         Some(syn::NestedMeta::Meta(syn::Meta::NameValue(meta_name))) => {
-                            if &meta_name.path.get_ident().map_or(String::from("undefined"), |id| id.to_string()) != "each" {
+                            if &meta_name
+                                .path
+                                .get_ident()
+                                .map_or(String::from("undefined"), |id| id.to_string())
+                                != "each"
+                            {
                                 return None;
                             }
-                            
+
                             if let syn::Lit::Str(lit_str) = &meta_name.lit {
                                 return Some(lit_str.value());
                             }
@@ -79,13 +94,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
         let name = &f.ident;
         let ty = &f.ty; //This variable is a Type
 
-        match is_an_option(f) {
-            Some(generic_arg) => {
+        match get_inner(f) {
+            InnerType::OptionInner(generic_arg) => {
                 quote! {
                     #name: std::option::Option#generic_arg
                 }
             }
-            None => {
+            _ => {
                 quote! {
                     #name: std::option::Option<#ty>
                 }
@@ -96,12 +111,46 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let fnized = fields.iter().map(|f| {
         let name = &f.ident;
         let mut ty = &f.ty;
-        
-        let is_option = is_an_option(f);
 
-        if let Some(ref generic_arg) = is_option {
+        let raw_name = if let Some(id) = &name {
+            id.to_string()
+        } else {
+            String::from("noname")
+        };
+
+        let is_option = get_inner(f);
+
+        if let InnerType::OptionInner(ref generic_arg) = is_option {
             if let syn::GenericArgument::Type(ref base_ty) = generic_arg.args.first().unwrap() {
                 ty = base_ty;
+            }
+        } else if let InnerType::VecInner(ref generic_arg) = is_option {
+            if let syn::GenericArgument::Type(ref base_ty) = generic_arg.args.first().unwrap() {
+                if let Some(inner_name) = has_attribute(f) {
+                    let inner_id = syn::Ident::new(&inner_name, proc_macro2::Span::call_site());
+                    if inner_name != raw_name {
+                        return quote! {
+                            pub fn #inner_id(&mut self, #name: #base_ty) -> &mut Self{
+                                let mut vec_values = self.#name.get_or_insert(Vec::new());
+                                vec_values.push(#name);
+                                self
+                            }
+
+                            pub fn #name(&mut self, #name: #ty) -> &mut Self{
+                                self.#name = Some(#name);
+                                self
+                            }
+                        };
+                    } else {
+                        return quote! {
+                            pub fn #inner_id(&mut self, #name: #base_ty) -> &mut Self{
+                                let mut vec_values = self.#name.get_or_insert(Vec::new());
+                                vec_values.push(#name);
+                                self
+                            }
+                        };
+                    }
+                }
             }
         }
 
@@ -116,13 +165,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let build_fn = fields.iter().map(|f| {
         let name = &f.ident;
 
-        match is_an_option(f) {
-            Some(_) => {
+        match get_inner(f) {
+            InnerType::OptionInner(_) => {
                 quote! {
                     #name: self.#name.clone()
                 }
             }
-            None => {
+            _ => {
                 quote! {
                     #name: self.#name.clone().ok_or(concat!("Missing ", stringify!(#name), " argument"))?
                 }
@@ -142,8 +191,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
             fn builder() -> #bident {
                 #bident{
                     executable: None,
-                    args: None,
-                    env: None,
+                    args: Some(Vec::new()),
+                    env: Some(Vec::new()),
                     current_dir: None,
                 }
             }
